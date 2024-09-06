e6869bdea057c021f8597b81dce2273b89b01bae
arifjamal90 <mohdarif727431@gmail.com>
2024-09-06 09:29:27 +0530

    first commit

---
+++ DOMTASK/script.js
@@ -0,0 +1,17 @@
+// const navbar = document.querySelectorAll("ul .nav-item ");
+// console.log(navbar);
+
+// const myDiv= document.createElement("div");
+// myDiv.classList.add("mainDive");
+
+// const myInput=document.createElement("input");
+// myInput.classList.add("text");
+// myInput.setAttribute("type","text");
+// myInput.readOnly=true;
+
+
+// const mainButton =  document.querySelectorAll(".main button");
+// console.log(mainButton[0]);
+// const buttonone= addEventListener("click", ()=>{
+//     console.log("hiiiii i am mohd arif");
+// })  
